9645bd496b3f2b3706b4ff2da934bde1aea15607
minjuhyun1999 <minjoohyun67@naver.com>
2025-11-06 10:42:11 +0900

    영어 -> 한글로 변경 기록 사용자별로 다르게 변경

--- team-project/src/components/MyPage.jsx
+++ team-project/src/components/MyPage.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './MyPage.css'; // CSS 파일 임포트
 import { FaUser, FaEnvelope, FaLock, FaIdBadge } from 'react-icons/fa'; // 아이콘 추가
 import { TbDatabaseSearch } from "react-icons/tb";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPersonDrowning } from '@fortawesome/free-solid-svg-icons';
 import SAGE_LOGO_URL from '../image/img.png';
 import './Header.css';
 import { Link } from 'react-router-dom';
@@ -76,7 +78,7 @@ const MyPage = () => {
     return (
         <div className="login-container">
             <Link to="/toolspage" style={{ textDecoration: 'none', color: 'inherit' }}>
-              <h1 className="project-title" style={{paddingBottom: '20px'}}><img src={SAGE_LOGO_URL} alt="Tsunami Prediction Logo" style={{ height: '24px', marginRight: '8px', verticalAlign: 'middle' }} /> Tsunami Prediction</h1>
+              <h1 className="project-title" style={{paddingBottom: '20px'}}><FontAwesomeIcon icon={faPersonDrowning} style={{ height: '24px', marginRight: '8px', verticalAlign: 'middle' }} /> Tsunami Prediction</h1>
             </Link>
             <div className="login-box">
                 <h1 className="login-title">마이페이지</h1>
--- team-project/src/components/PageInfo.jsx
+++ team-project/src/components/PageInfo.jsx
@@ -7,11 +7,11 @@ const PageInfo = ({ historyLinkRef, handleShowHistory }) => {
                 <a href="#data-services">DATA SERVICES</a>
             </div>
             <div className="page-title-row">
-                <h1 className="page-title" style={{ fontSize: '2em' }}>World Map</h1>
+                <h1 className="page-title" style={{ fontSize: '2em' }}>지도</h1>
                 <div className="wilber-support">? Wilber Support</div>
             </div>
             <div className="page-description">
-                <a href="#" ref={historyLinkRef} onClick={handleShowHistory}>History</a>
+                <a href="#" ref={historyLinkRef} onClick={handleShowHistory}>기록</a>
             </div>
         </div>
     );
